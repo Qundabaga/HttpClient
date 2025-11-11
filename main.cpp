f62cfea601cb5f416649a724e069c0a5e4cbe238
Armands Kurevskis <armandskur@gmail.com>
2025-11-11 22:41:43 +0200

    Finished hostname validation

--- main.cpp
+++ main.cpp
@@ -39,66 +39,54 @@ int main(const int argc, char* argv[] ) {
         return EXIT_FAILURE;
     }
 
-    /*
-    //Check if there are no double dots or dot at the end
-    //Check if current i pos has '.', if yes then reject
-    //move the i to the next dot, after i++ will move it to next. If we see another dot then reject
-    for (int i = 0; i < hostname.size(); i++) {
-        if (hostname[i] == '.') {
-            std::cerr << "Invalid hostname: empty label (consecutive or leading dot)." << std::endl;
-            return EXIT_FAILURE;
-        }
-        if (hostname.find('.',i) != std::string::npos) {
-            i = hostname.find('.',i);
-        }
-    }
-    */
-
     //Create a view which will be used to determine the correctness of the hostname
     std::string_view view = hostname;
-    //In case if view ends with '.' then remove it. It should still work properly.
-    if (view.rfind('.') == view.size()-1) {
-        if (view.size() == 0) {
+    //In case if view ends with '.' then remove it for next checkup stages
+    if (view.ends_with('.')) {
+        //If the size of the view is 1, then the resulting string is empty and should be rejected
+        if (view.size() == 1) {
             std::cerr << "Invalid hostname: empty label (consecutive or leading dot)." << std::endl;
             return EXIT_FAILURE;
         }
         view.remove_suffix(1);
     }
-    //Check if there are no double dots or dot at the end
-    //Check if current i pos has '.', if yes then reject
-    //move the i to the next dot, after i++ will move it to snext. If we see another dot then reject
-    //example.com.s
 
-    std::string_view test = "example.com.s";
-
-    std::cout << test.substr(8,11-8) << std::endl;
-
-    std::cout << view.find('.',12) << std::endl;
-
+    //Initialize iterators used in splitting strings into labels
+    //First label start is start of the string
     size_t labelStart = 0;
     size_t labelEnd = 0;
     do {
+        //Find where the label ends (it's either next dot or end of string if no dots remain)
         labelEnd = view.find('.',labelStart);
+        //Save label in separate string_view
         std::string_view labelView = view.substr(labelStart,labelEnd-labelStart);
-        std::cout << "labelView: " << labelView << std::endl;
+        //Reject incorrect labels
+        //Empty label
+        if (labelView.empty()) {
+            std::cerr << "Invalid hostname: empty label (consecutive or leading dot)." << std::endl;
+            return EXIT_FAILURE;
+        }
+        //Label size 1-63
+        if (labelView.size() > 63) {
+            std::cerr << "Invalid hostname: Label length: 1–63." << std::endl;
+            return EXIT_FAILURE;
+        }
+        //Label should not start or end with '-'
+        if (labelView.ends_with('-') || labelView.starts_with('-')) {
+            std::cerr << "Invalid hostname: No leading or trailing - in label." << std::endl;
+            return EXIT_FAILURE;
+        }
 
+        if (!std::ranges::all_of(labelView, [](unsigned char c){return std::isalnum(c) || c == '-';})) {
+            std::cerr << "Invalid hostname: Allowed chars per label: [A–Z a–z 0–9 -]." << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        //In case if it's the last label
         if (labelEnd == std::string_view::npos) {break;}
+        //When looking for next label then it starts with the next char after the end of last one
         labelStart = labelEnd + 1;
     } while (true);
-    /*
-    for (size_t i = 0; i < view.size(); i++) {
-        size_t labelStart = i;
-        std::cout << "view: " << view << std::endl;
-        std::cout << "i " << i << std::endl;
-        size_t labelEnd = view.find('.',i);
-        std::cout << "labelEnd: " << labelEnd << std::endl;
-        std::string_view labelView = view.substr(labelStart, labelEnd-labelStart);
-        std::cout << labelView << std::endl;
-        std::cout << labelEnd << std::endl;
-        std::cout << i << std::endl;
-
-        i = labelEnd;
-    }*/
 
     std::cout << "Host: " << argv[1] << std::endl;
     return 0;
